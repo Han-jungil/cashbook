e8897aae9a8b187b6ea10a4c093ae0f21560ab50
Hanji55 <Hanji55@DESKTOP-IQ29D7P>
2022-04-18 17:19:11 +0900

    [한정일] vo.Cashbook 생성, dao.CashbookDao selectCashbookListByMonth생성, CashBookListByMonthController (요청값, 모델값)생성, view(CashBookListByMonthController.jsp)생성

---
+++ cashbook/src/main/java/vo/Cashbook.java
@@ -0,0 +1,61 @@
+package vo;
+
+public class Cashbook {
+	private int cashbookNo;
+	private String cashDate;
+	private String kind;
+	private int cash;
+	private String updateDate;
+	private String createDate;
+	private String memo;
+	//-------------------------------------- toString
+	@Override
+    public String toString() {
+        return "cashbook [cashbookNo=" + cashbookNo + ", cashDate=" + cashDate + ", kind=" + kind + ", memo=" + memo
+                + ", updateDate=" + updateDate + ", createDate=" + createDate + "]";
+    }
+	//--------------------------------------
+	public int getCashbookNo() {
+		return cashbookNo;
+	}
+	public void setCashbookNo(int cashbookNo) {
+		this.cashbookNo = cashbookNo;
+	}
+	public String getCashDate() {
+		return cashDate;
+	}
+	public void setCashDate(String cashDate) {
+		this.cashDate = cashDate;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public int getCash() {
+		return cash;
+	}
+	public void setCash(int cash) {
+		this.cash = cash;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
+}
